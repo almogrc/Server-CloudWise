d7b50953f775108e664333e7a7f9ec79a50f3069
Guy Yomtovian <guyym@mta.ac.il>
2023-05-25 11:36:27 +0300

    upgrate buissnes logic to .net 6.0 and update logger file

--- BuisnessLogic/Algorithms/ArimaAlgo.cs
+++ BuisnessLogic/Algorithms/ArimaAlgo.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Accord.Statistics.Models.Regression.Linear;
-using Accord.Math.Optimization.Losses;
-using Accord.Statistics.Models.Regression;
+
 namespace BuisnessLogic.Algorithms
 {
     internal class ArimaAlgo : IPredictiveAlgorithm
